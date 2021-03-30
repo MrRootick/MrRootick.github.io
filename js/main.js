846e78b86e6356be7f28afe5bf051b2d4c271fcd
MrRootick <31653337+MrRootick@users.noreply.github.com>
2021-03-30 11:35:12 +0300

    Первый вариант

---
+++ js/main.js
@@ -0,0 +1,10 @@
+butt.onclick = function(){
+var name = document.getElementById('name').value;
+var address = document.getElementById('address').value;
+var tel = document.getElementById('tel').value;
+var mail = document.getElementById('mail').value;
+document.getElementById('str1').innerHTML="Вы ввели имя: "+name;
+document.getElementById('str2').innerHTML="Вы ввели адрес: "+address;
+document.getElementById('str3').innerHTML="Вы ввели номер телефона: "+tel;
+document.getElementById('str4').innerHTML="Вы ввели почту: "+mail;
+	};
