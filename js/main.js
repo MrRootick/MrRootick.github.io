8b04afd7dc98a4ddbecb429a6b9da37d81c745b5
MrRootick <31653337+MrRootick@users.noreply.github.com>
2021-03-30 14:46:39 +0300

    .

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ var name = document.getElementById('name').value;
 var address = document.getElementById('address').value;
 var tel = document.getElementById('tel').value;
 var mail = document.getElementById('mail').value;
-document.getElementById('str1').innerHTML="Вы ввели имя: "+name;
-document.getElementById('str2').innerHTML="Вы ввели адрес: "+address;
-document.getElementById('str3').innerHTML="Вы ввели номер телефона: "+tel;
-document.getElementById('str4').innerHTML="Вы ввели почту: "+mail;
+if (name!='' && address!='' && tel!='' && mail!=''){
+	document.getElementById('str1').innerHTML="Вы ввели имя: "+name;
+	document.getElementById('str2').innerHTML="Вы ввели адрес: "+address;
+	document.getElementById('str3').innerHTML="Вы ввели номер телефона: "+tel;
+	document.getElementById('str4').innerHTML="Вы ввели почту: "+mail;
+}
+else {
+	document.getElementById('str1').innerHTML="Не все поля заполнены";
 	};
+}
